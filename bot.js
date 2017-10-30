73ff5e6d9cc961a5028040109424aca4bf8c47e9
Acezus <32319352+Acezus@users.noreply.github.com>
2017-10-30 20:56:22 +1100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,25 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
-    console.log('I am ready!');
+client.on("ready", () => {
+  client.user.setGame(`I run for 24/7, Unless updates needed`);
+  console.log(`Ready to serve on ${client.guilds.size} servers, for ${client.users.size} users.`);
 });
 
- const cmdFiles = await readdir("./commands/");
-  client.log("log", `Loading a total of ${cmdFiles.length} commands.`);
-  cmdFiles.forEach(f => {
-    if (!f.endsWith(".js")) return;
-    const response = client.loadCommand(f);
-    if (response) console.log(response);
-  });
+var prefix = '!';
+function isCommand(command, message){
+	var command = command.toLowerCase();
+	var content = message.content.toLowerCase();
+	return content.startsWith(prefix + command);
+}
+
+client.on('message', (message) => {
+	if (message.author.bot) return; // Dont answer yourself.
+    var args = message.content.split(/[ ]+/)
+    if(isCommand('Hello', message)){
+    	message.reply("Move Along!")
+    }
+});
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
